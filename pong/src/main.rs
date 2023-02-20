b50219f46c8527c4ff8a078eea19eb33bb0a9606
rhammock1 <robert.hammock@gotab.io>
2023-02-20 09:10:03 -0500

    impl draw method

--- pong/src/main.rs
+++ pong/src/main.rs
@@ -3,7 +3,13 @@ use tetra::{Context, ContextBuilder, State};
 
 struct GameState {}
 
-impl State for GameState {}
+impl State for GameState {
+  fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
+    graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+
+    Ok(())
+  }
+}
 
 fn main() -> tetra::Result {
     ContextBuilder::new("Pong", 640, 480)
