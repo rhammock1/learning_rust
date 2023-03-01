ff6d04fb6c3a1230fb4ef7affb89d7b63bd18c68
rhammock1 <robert.hammock@gotab.io>
2023-02-28 20:48:52 -0500

    Cleanup loop. Still not ideal

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -6,39 +6,31 @@ use std::{
 /// Parses the program arguments and returns a tuple of the query, filepath, and regex
 /// # Arguments
 /// * `args` - A vector of the program arguments
-fn parse_program_args(args: Vec<String>) -> (String, String, String) {
+fn parse_program_args(args: &mut Vec<String>) -> (String, String, String) {
   let mut query: String = String::new();
   let mut filepath: String = String::new();
   let mut regex: String = String::new();
-  for (i, arg) in args.iter().enumerate() {
+
+  // This still isn't ideal, but we aren't using any external libraries
+  for i in 1..args.len() - 1 {
     // options generally come before the 
     // query and file path arguments
 
-    if i == 0 {
-      // skip the first argument, which is the program name
-      continue;
-    }
-
-    // I don't know how to handle this yet, but this if statement works for now
-    if args[i - 1] == "-r" || args[i - 1] == "--regex" {
-      println!("Regex was found previously. Skipping...");
-      continue;
-    }
-
-    match arg.as_str() {
+    match args[i].as_str() {
       "-r" | "--regex" => {
         println!("Regex option found: {}", args[i + 1].to_string());
         regex = args[i + 1].to_string();
+        args.remove(i + 1);
       },
       _ => {
-        let path = Path::new(&arg);
+        let path = Path::new(&args[i]);
         println!("File: {:?}", path);
         if path.exists() {
-          println!("Filepath found: {}", arg.to_string());
-          filepath = arg.to_string();
+          println!("Filepath found: {}", args[i].to_string());
+          filepath = args[i].to_string();
         } else {
-          println!("Query found: {}", arg);
-          query = arg.to_string();
+          println!("Query found: {}", args[i]);
+          query = args[i].to_string();
         }
       },
     }
@@ -49,9 +41,9 @@ fn parse_program_args(args: Vec<String>) -> (String, String, String) {
 fn main() {
   // env::args() can only accept unicode values
   // env::args_os() can accept any value (but returns an OsString, which is harder to work with)
-  let args: Vec<String> = env::args().collect();
+  let mut args: Vec<String> = env::args().collect();
 
-  let (query, filepath, regex) = parse_program_args(args);
+  let (query, filepath, regex) = parse_program_args(&mut args);
 
   println!("Query: {}, Filepath: {}, Regex: {}", query, filepath, regex);
 }
