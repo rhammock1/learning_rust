7600064bf619f6175d891deabfc577c90340cc66
rhammock1 <robert.hammock@gotab.io>
2023-02-28 21:02:13 -0500

    separate logic from main

---
+++ greprs/src/lib.rs
@@ -0,0 +1,56 @@
+use std::{
+  fs,
+  path::Path,
+};
+
+pub struct Config {
+  pub query: String,
+  pub filepath: String,
+  pub regex: String,
+}
+
+impl Config {
+  pub fn new(args: &Vec<String>) -> Config {
+    let mut query: String = String::new();
+    let mut filepath: String = String::new();
+    let mut regex: String = String::new();
+
+    // This still isn't ideal, but we aren't using any external libraries
+    for i in 1..args.len() {
+      // options generally come before the 
+      // query and file path arguments
+
+      // Back to this for now
+      if args[i - 1].as_str() == "-r" || args[i - 1].as_str() == "--regex" {
+        continue;
+      }
+
+      match args[i].as_str() {
+        "-r" | "--regex" => {
+          println!("Regex option found: {}", args[i + 1].to_string());
+          regex = args[i + 1].clone().to_string();
+        },
+        _ => {
+          let path = Path::new(&args[i]);
+          println!("File: {:?}", path);
+          if path.exists() {
+            println!("Filepath found: {}", args[i].to_string());
+            filepath = args[i].clone().to_string();
+          } else {
+            println!("Query found: {}", args[i]);
+            query = args[i].clone().to_string();
+          }
+        },
+      }
+    }
+
+    Config { query, filepath, regex }
+  }
+}
+
+pub fn read_file_contents(filepath: &String) -> String {
+  let contents = fs::read_to_string(filepath)
+    .expect("Something went wrong reading the file");
+
+  contents
+}
--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -1,49 +1,14 @@
-use std::{
-  env,
-  path::Path,
-};
-
-/// Parses the program arguments and returns a tuple of the query, filepath, and regex
-/// # Arguments
-/// * `args` - A vector of the program arguments
-fn parse_program_args(args: &mut Vec<String>) -> (String, String, String) {
-  let mut query: String = String::new();
-  let mut filepath: String = String::new();
-  let mut regex: String = String::new();
-
-  // This still isn't ideal, but we aren't using any external libraries
-  for i in 1..args.len() - 1 {
-    // options generally come before the 
-    // query and file path arguments
-
-    match args[i].as_str() {
-      "-r" | "--regex" => {
-        println!("Regex option found: {}", args[i + 1].to_string());
-        regex = args[i + 1].to_string();
-        args.remove(i + 1);
-      },
-      _ => {
-        let path = Path::new(&args[i]);
-        println!("File: {:?}", path);
-        if path.exists() {
-          println!("Filepath found: {}", args[i].to_string());
-          filepath = args[i].to_string();
-        } else {
-          println!("Query found: {}", args[i]);
-          query = args[i].to_string();
-        }
-      },
-    }
-  }
-  (query, filepath, regex)
-}
+use std::env;
+use greprs::{Config, read_file_contents};
 
 fn main() {
   // env::args() can only accept unicode values
   // env::args_os() can accept any value (but returns an OsString, which is harder to work with)
-  let mut args: Vec<String> = env::args().collect();
+  let args: Vec<String> = env::args().collect();
 
-  let (query, filepath, regex) = parse_program_args(&mut args);
+  let config = Config::new(&args);
 
-  println!("Query: {}, Filepath: {}, Regex: {}", query, filepath, regex);
+  let contents = read_file_contents(&config.filepath);
+
+  println!("With text:\n{}", contents);
 }
